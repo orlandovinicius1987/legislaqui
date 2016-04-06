a88f94266d4fba95e58d24351782c069178c9315
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-04-06 19:10:54 +0000

    phpunit test ok

--- tests/ProposalsTest.php
+++ tests/ProposalsTest.php
@@ -6,16 +6,15 @@ use App\User;
 
 use Faker\Factory;
 
+use Anhskohbo\NoCaptcha\Facades\NoCaptcha;
+use  Illuminate\Contracts\Auth\Authenticatable;
+
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ProposalsTest extends TestCase
 {
-    //use DatabaseMigrations;
-    //use DatabaseTransactions;
-    //use WithoutMiddleware;
-
     /**
      * A basic functional test example.
      *
@@ -30,7 +29,10 @@ class ProposalsTest extends TestCase
         $proposal = factory(App\Proposal::class)->create();
 
         $this->visit('/proposals')
-            ->see($proposal->name);
+            ->seeInDatabase('proposals', ['name' => $proposal->name]);
+
+        //Not with Paginator
+        //->see($proposal->name);
     }
 
     public function testLogin()
@@ -50,16 +52,21 @@ class ProposalsTest extends TestCase
 
     public function testProposalsControllerIndex()
     {
+        //Visit /proposals
         $response = $this->call('GET', 'proposals');
+
         $this->assertViewHas('proposals');
         // getData() returns all vars attached to the response.
         $proposals = $response->original->getData()['proposals'];
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $proposals);
+
+        // When calling the paginate method, you will receive an instance of Illuminate\Pagination\LengthAwarePaginator
+        $this->assertInstanceOf('Illuminate\Pagination\LengthAwarePaginator', $proposals);
+        //$this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $proposals);
     }
 
     public function testClickProposal()
     {
-        $proposal = Proposal::all()->shuffle()->first();
+        $proposal = Proposal::paginate(20)->shuffle()->first();
         $url_proposal = '/proposals/' . $proposal->id;
 
         $this->visit('/proposals/')
@@ -72,13 +79,23 @@ class ProposalsTest extends TestCase
 
     public function testRegisterAction ()
     {
-        //use the factory to create a Faker\Generator instance
+        // use the factory to create a Faker\Generator instance
         $faker = Faker\Factory::create();
         //Generate a User Data to Register
         $name = $faker->name($gender = null|'male'|'female');
         $email = $faker->freeEmail();
         $pwd = '123456';
         $state = State::all()->random();
+
+        // prevent validation error on captcha
+        NoCaptcha::shouldReceive('verifyResponse')
+            ->once()
+            ->andReturn(true);
+
+        // provide hidden input for your 'required' validation
+        NoCaptcha::shouldReceive('display')
+            ->zeroOrMoreTimes()
+            ->andReturn('<input type="hidden" name="g-recaptcha-response" value="1" />');
 
         $this->visit('/')
             ->click('Registro')
@@ -112,11 +129,14 @@ class ProposalsTest extends TestCase
     {
         //use the factory to create a Faker\Generator instance
         $faker = Faker\Factory::create();
-        //generate a proposal response
+
+        //generate a 6 words proposal response
         $response = $faker->sentence($nbWords = 6, $variableNbWords = true);
+
         //get first admin user in DB
-        $user = User::all()->where('is_admin', true)->first();
-        //authenticate the given user, navigate thru not responded menu, fill proposal response, see if it's in DB
+        $user = User::where('role_id', 0)->first();
+
+        //authenticate the given user -> navigate through not responded menu -> fill proposal response -> see if it's in DB
         $this->actingAs($user)
             ->visit('/')
             ->click('Sem Resposta')
@@ -132,7 +152,7 @@ class ProposalsTest extends TestCase
     public function testProposalPaginates()
     {
         factory(App\Proposal::class, 50)->create();
-        $proposals = App\Proposal::paginate(10);
-        $this->assertEquals(10, $proposals->count());
+        $proposals = App\Proposal::paginate(20);
+        $this->assertEquals(20, $proposals->count());
     }
 }
