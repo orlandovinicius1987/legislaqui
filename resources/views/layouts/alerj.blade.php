ad608bf3f9c0e084c8ca93fbfd4b6148a57b1f8f
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-04-08 15:58:05 +0000

    footer social images

--- resources/views/layouts/alerj.blade.php
+++ resources/views/layouts/alerj.blade.php
@@ -264,11 +264,11 @@
             <p>Rua Primeiro de março, s/n - Praça XV - Rio de Janeiro<br /><span>CEP</span> 20010-090 &nbsp;&nbsp; <span>Telefone</span> +55 (21) 2588-1000 &nbsp;&nbsp; <span>Fax</span> +55 (21) 2588-1516</p>
         </div>
         <div class="redes_sociais">
-            <div class="logotipo_redes"><a target="_blank" href="https://instagram.com/instalerj/" title="Instagram"><img src="/Content/imagens/icone_36.png" width="20" height="20" alt="Instagram" title="Instagram"></a></div>
-            <div class="logotipo_redes"><a target="_blank" href="https://www.facebook.com/assembleiaRJ" title="Facebook"><img src="/Content/imagens/icone_06.png" width="20" height="20" alt="Facebook" title="Facebook"></a></div>
-            <div class="logotipo_redes"><a target="_blank" href="https://plus.google.com/u/0/+alerj/posts"><img src="/Content/imagens/icone_07.png" width="20" height="20" alt="Google Mais" title="Google Mais"></a></div>
-            <div class="logotipo_redes"><a target="_blank" href="https://twitter.com/alerj" title="Twitter"><img src="/Content/imagens/icone_08.png" width="20" height="20" alt="Twitter" title="Twitter"></a></div>
-            <div class="logotipo_redes"><a target="_blank" href="https://www.youtube.com/user/dcsalerj" title="Youtube"><img src="/Content/imagens/icone_09.png" width="20" height="20" alt="Youtube" title="Youtube"></a></div>
+            <div class="logotipo_redes"><a target="_blank" href="https://instagram.com/instalerj/" title="Instagram"><img src="http://www.alerj.rj.gov.br/Content/imagens/icone_36.png" width="20" height="20" alt="Instagram" title="Instagram"></a></div>
+            <div class="logotipo_redes"><a target="_blank" href="https://www.facebook.com/assembleiaRJ" title="Facebook"><img src="http://www.alerj.rj.gov.br/Content/imagens/icone_06.png" width="20" height="20" alt="Facebook" title="Facebook"></a></div>
+            <div class="logotipo_redes"><a target="_blank" href="https://plus.google.com/u/0/+alerj/posts"><img src="http://www.alerj.rj.gov.br/Content/imagens/icone_07.png" width="20" height="20" alt="Google Mais" title="Google Mais"></a></div>
+            <div class="logotipo_redes"><a target="_blank" href="https://twitter.com/alerj" title="Twitter"><img src="http://www.alerj.rj.gov.br/Content/imagens/icone_08.png" width="20" height="20" alt="Twitter" title="Twitter"></a></div>
+            <div class="logotipo_redes"><a target="_blank" href="https://www.youtube.com/user/dcsalerj" title="Youtube"><img src="http://www.alerj.rj.gov.br/Content/imagens/icone_09.png" width="20" height="20" alt="Youtube" title="Youtube"></a></div>
         </div>
     </div>
 </div>
