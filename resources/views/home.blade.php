2d6c4b53ce2fdbfaaac34a96344c81c06fea3ed6
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-07-19 17:48:58 +0000

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.alerj')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+<div class="home-content">
+    <br>
+    <br>
+        <div class="row">
+            <div class="col-sm-4 col-xs-12">
+                <div class="dummy"></div>
+                <a href="/proposals/create" class="square">
+                    <div class="icon"><i class="fa fa-plus" aria-hidden="true"></i></div>Crie uma Ideia Legislativa</a>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="dummy"></div>
+                <a href="/proposals" class="square">
+                    <div class="icon"><i class="fa fa-star" aria-hidden="true"></i></div>Apoie uma Proposta</a>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <div class="dummy"></div>
+                <a href="/about" class="square">
+                    <div class="icon"><i class="fa fa-question" aria-hidden="true"></i></div>Como Funciona</a>
             </div>
         </div>
-    </div>
 </div>
 @endsection
