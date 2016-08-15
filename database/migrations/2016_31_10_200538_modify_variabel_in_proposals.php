84d9bae2d2fc22c1b0976c3da2691eceac12e472
FredericoChamps <testa@testando.com>
2016-08-15 21:42:15 +0000

    Modify_Variabel_In_Committee

---
+++ database/migrations/2016_31_10_200538_modify_variabel_in_proposals.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyVariabelInProposals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('proposals', function ($table) {
+            $table->boolean('in_committee')->default(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('proposals', function ($table) {
+            $table->dropColumn('in_committee');
+        });
+
+    }
+}
+
