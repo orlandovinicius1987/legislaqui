391b2540feaaa81c176be71e618446e7be9c6dbf
falbernaz <fabiano.albernaz@gmail.com>
2016-09-08 16:39:05 +0000

    Search in Green: CSS

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,23 @@
-<div class="row">
-    <div class="col-xs-12 col-sm-8 pull-right">
-        {!! Form::open(array('route' => 'home.post', 'class'=>'form')) !!}
-        {!! Form::text('search', null,
-            array('required',
-            'class'=>'form-control',
-            'placeholder'=>'Busque uma ideia ...')) !!}
+<div class="header">
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 pull-right">
+            {!! Form::open(array('route' => 'home.post', 'class'=>'form')) !!}
+            {!! Form::text('search', null,
+                array('required',
+                'class'=>'form-control',
+                'placeholder'=>'Busque uma ideia ...')) !!}
 
-        <span class="glyphicon glyphicon-search form-control-feedback"></span>
-        {!! Form::close() !!}
+            <span class="glyphicon glyphicon-search form-control-feedback"></span>
+            {!! Form::close() !!}
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-xs-12 col-sm-8 pull-right" role="group" aria-label="...">
-        <a href="/?q=open" class="btn btn-default {{ $query == null ? 'active' :'' }} {{ $query == "open" ? 'active' :'' }}"> Abertas</a>
-        <a href="/?q=committee" class="btn btn-default {{ $query == "committee" ? 'active' :'' }}">Na comissão</a>
-        <a href="/?q=expired" class="btn btn-default {{ $query == "expired" ? 'active' :'' }}">Expiradas</a>
-        <a href="/?q=disapproved" class="btn btn-default {{ $query == "disapprove" ? 'active' :'' }}">Não acatadas</a>
-        <a href="/?q=approved" class="btn btn-default {{ $query == "approved" ? 'active' :'' }}">Aprovadas</a>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 pull-right" role="group" aria-label="...">
+            <a href="/?q=open" class="btn btn-default {{ $query == null ? 'active' :'' }} {{ $query == "open" ? 'active' :'' }}"> Abertas</a>
+            <a href="/?q=committee" class="btn btn-default {{ $query == "committee" ? 'active' :'' }}">Na comissão</a>
+            <a href="/?q=expired" class="btn btn-default {{ $query == "expired" ? 'active' :'' }}">Expiradas</a>
+            <a href="/?q=disapproved" class="btn btn-default {{ $query == "disapprove" ? 'active' :'' }}">Não acatadas</a>
+            <a href="/?q=approved" class="btn btn-default {{ $query == "approved" ? 'active' :'' }}">Aprovadas</a>
+        </div>
     </div>
 </div>
