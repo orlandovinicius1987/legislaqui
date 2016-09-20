f46c5b7b7f5346a8e8bd24e5ff14903d12988e28
Felipe Feder <felipeoliveirafeder@hotmail.com>
2016-09-20 15:49:10 +0000

    ToolTips_Msg_Filters

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,36 @@
             {!! Form::close() !!}
         </div>
     </div>
-    <div class="row">
+
+    <button type="button" class="btn btn-default {{ $query == null ? 'active' :'' }} {{ $query == "open" ? 'active' :'' }}"
+            data-toggle="tooltip" data-placement="bottom"
+            title="Essas são as propostas que ainda não chegaram à Comissão. Uma proposta precisa de 20 mil apoios para ser
+                encaminhada à Comissão. Antes de criar uma proposta, verifique se não há uma já criada para o mesmo fim.
+                 Várias ideias semelhantes terminam diluindo o apoio dos demais cidadãos.">
+        <a href="/?q=open"> ABERTAS</a></button>
+
+    <button type="button" class="btn btn-default {{ $query == "committee" ? 'active' :'' }}"
+            data-toggle="tooltip" data-placement="bottom"
+            title="Essas são as propostas que receberam o apoio suficiente e, neste momento, estão sendo analisadas pela comissão.">
+        <a href="/?q=committee">NA COMISSÃO</a></button>
+
+    <button type="button" class="btn btn-default {{ $query == "expired" ? 'active' :'' }}"
+            data-toggle="tooltip" data-placement="bottom"
+            title="Essas são as propostas que não receberam o apoio suficiente e não foram encaminhadas para análise da comissão.">
+        <a href="/?q=expired">EXPIRADAS</a></button>
+
+    <button type="button" class="btn btn-default {{ $query == "disapproved" ? 'active' :'' }}"
+            data-toggle="tooltip" data-placement="bottom"
+            title="Essas são as propostas analisadas e não acatadas pela comissão.">
+        <a href="/?q=disapproved">NÃO ACATADAS</a></button>
+
+    <button type="button" class="btn btn-default {{ $query == "approved" ? 'active' :'' }}"
+            data-toggle="tooltip" data-placement="bottom"
+            title="Essas são as propostas analisadas e aprovadas pela comissão.">
+        <a href="/?q=approved">APROVADAS</a></button>
+
+    {{-- Don't remove before warning --}}
+    {{--<div class="row">
         <div class="col-xs-12" role="group" aria-label="...">
             <a href="/?q=open" class="btn btn-default {{ $query == null ? 'active' :'' }} {{ $query == "open" ? 'active' :'' }}"> ABERTAS</a>
             <a href="/?q=committee" class="btn btn-default {{ $query == "committee" ? 'active' :'' }}">NA COMISSÃO</a>
@@ -19,5 +48,5 @@
             <a href="/?q=disapproved" class="btn btn-default {{ $query == "disapproved" ? 'active' :'' }}">NÃO ACATADAS</a>
             <a href="/?q=approved" class="btn btn-default {{ $query == "approved" ? 'active' :'' }}">APROVADAS</a>
         </div>
-    </div>
+    </div>--}}
 </div>
