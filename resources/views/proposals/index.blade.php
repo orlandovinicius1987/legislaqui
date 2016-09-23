9d33790190275900eb67679b793852ec8ba537b2
Felipe Feder <felipeoliveirafeder@hotmail.com>
2016-09-23 15:48:32 +0000

    Links_in_Dropdown_Mobile+CSS

--- resources/views/proposals/index.blade.php
+++ resources/views/proposals/index.blade.php
@@ -96,22 +96,41 @@
                             <a href="/?q=approved">APROVADAS</a></button>
                     </div>--}}
                 </div>
-                <div class="row visible-xs">
+
+                {{--<div class="row visible-xs">
                     <div class="wrap-select">
                         <div id="dd" class="wrapper-dropdown-3">
                             <span>Filtre sua busca...</span>
                             <ul class="dropdown">
-                                <li><a href="#">Abertas</a></li>
-                                <li><a href="#">Na Comissão</a></li>
-                                <li><a href="#">Expiradas</a></li>
-                                <li><a href="#">Não Acatadas</a></li>
-                                <li><a href="#">Aprovadas</a></li>
+                                <li><a href="/?q=open">Abertas</a></li>
+                                <li><a href="/?q=committee">Na Comissão</a></li>
+                                <li><a href="/?q=expired">Expiradas</a></li>
+                                <li><a href="/?q=disapproved">Não Acatadas</a></li>
+                                <li><a href="/?q=approved">Aprovadas</a></li>
                             </ul>
                         </div>
                     </div>
+                </div>--}}
+
+ {{-- COMPARE - AINDA NÃO CONCLUIDO  --}}
+
+                <div class="row visible-xs">
+                    <div class="dropdown btn-group">
+                        <button class="btn gradient dropdown-toggle drop_filter" data-toggle="dropdown" id="filters">
+                            Filtre sua busca
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="/?q=open">Abertas</a></li>
+                            <li><a href="/?q=committee">Na Comissão</a></li>
+                            <li><a href="/?q=expired">Expiradas</a></li>
+                            <li><a href="/?q=disapproved">Não Acatadas</a></li>
+                            <li><a href="/?q=approved">Aprovadas</a></li>
+                        </ul>
+                    </div>
+                 </div>
 
 
-                </div>
 
                 {{--{!! $proposals->links() !!}--}}
             </div>
