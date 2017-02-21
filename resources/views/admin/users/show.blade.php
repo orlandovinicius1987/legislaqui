de2aa7e7891af2490712d210d421de6a9c225a46
Felipe Feder <felipeoliveirafeder@hotmail.com>
2017-02-21 14:34:53 +0000

    Refresh views

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -47,7 +47,7 @@
 
                         @can('edit', Auth::user())
                             <div class="col-md-12 botao">
-                                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary botao">
+                                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary botao" id="editarUsuario">
                                     <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar Usuário</a>
                             </div>
 
