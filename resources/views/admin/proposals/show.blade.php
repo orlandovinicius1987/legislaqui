a874f20d1a1dd1cc598fe2a93af0e87bfa71b123
Felipe Feder <felipeoliveirafeder@hotmail.com>
2017-08-01 15:37:31 +0000

    Refresh button to send a committee

--- resources/views/admin/proposals/show.blade.php
+++ resources/views/admin/proposals/show.blade.php
@@ -45,53 +45,66 @@
 
                                     <div class="panel-footer">
                                         <div class="row">
-                                        {{--Proposal Moderation--}}
-                                        <div class="pull-left botao">
-                                            @if ($proposal->approved_at == null && $proposal->approved_by == null && $proposal->disapproved_at == null && $proposal->disapproved_by == null )
-                                                <a href="{{ route('admin.proposal.response', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">
-                                                    <i class="fa fa-cog fa-spin fa fa-fw"></i> Moderar essa Ideia! </a>
-                                            @else
-                                                <i class="fa fa-check-square text-success" aria-hidden="true"></i> Esta Ideia Legislativa já foi Moderada.
-                                            @endif
-                                        </div>
+                                            {{--Proposal Moderation--}}
+                                            <div class="pull-left botao">
+                                                @if ($proposal->approved_at == null && $proposal->approved_by == null && $proposal->disapproved_at == null && $proposal->disapproved_by == null )
+                                                    <a href="{{ route('admin.proposal.response', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">
+                                                        <i class="fa fa-cog fa-spin fa fa-fw"></i> Moderar essa Ideia! </a>
+                                                @else
+                                                    <i class="fa fa-check-square text-success" aria-hidden="true"></i> Esta Ideia Legislativa já foi Moderada.
+                                                @endif
+                                            </div>
 
-                                        {{--<div class="pull-left botao">--}}
-                                            {{--<a href="{{ route('admin.proposal.approved', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
-                                                {{--<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> Aprovar! </a>--}}
-                                            {{--<a href="{{ route('admin.proposal.disapproved', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
-                                                {{--<span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> Desaprovar</a>--}}
-                                            {{--<a href="{{ route('proposal.approval', $proposal->id) }}" class="btn btn-info botao" role="button">--}}
-                                                {{--<span class="glyphicon glyphicon-star" aria-hidden="true"></span> Apoiar essa Ideia!</a>--}}
-                                        {{--</div>--}}
+                                            {{--<div class="pull-left botao">--}}
+                                                {{--<a href="{{ route('admin.proposal.approved', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
+                                                    {{--<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> Aprovar! </a>--}}
+                                                {{--<a href="{{ route('admin.proposal.disapproved', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
+                                                    {{--<span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> Desaprovar</a>--}}
+                                                {{--<a href="{{ route('proposal.approval', $proposal->id) }}" class="btn btn-info botao" role="button">--}}
+                                                    {{--<span class="glyphicon glyphicon-star" aria-hidden="true"></span> Apoiar essa Ideia!</a>--}}
+                                            {{--</div>--}}
 
-                                        {{--<div class="pull-left botao">--}}
+                                            {{--<div class="pull-left botao">--}}
 
-                                            {{--<a href="{{ URL::current() }}/like" class="btn btn-info" role="button">Apoiar essa Ideia!</a>--}}
-                                            {{--<a href="{{ route('proposal.like', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
-                                                {{--<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> Gostei dessa Ideia! </a>--}}
-                                            {{--<a href="{{ route('proposal.unlike', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
-                                                {{--<span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> Não gostei dessa Ideia!</a>--}}
-                                            {{--<a href="{{ route('proposal.approval', $proposal->id) }}" class="btn btn-info botao" role="button">--}}
-                                                {{--<span class="glyphicon glyphicon-star" aria-hidden="true"></span> Apoiar essa Ideia!</a>--}}
-                                        {{--</div>--}}
+                                                {{--<a href="{{ URL::current() }}/like" class="btn btn-info" role="button">Apoiar essa Ideia!</a>--}}
+                                                {{--<a href="{{ route('proposal.like', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
+                                                    {{--<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> Gostei dessa Ideia! </a>--}}
+                                                {{--<a href="{{ route('proposal.unlike', ['id' => $proposal->id]) }}" class="btn btn-info botao" role="button">--}}
+                                                    {{--<span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> Não gostei dessa Ideia!</a>--}}
+                                                {{--<a href="{{ route('proposal.approval', $proposal->id) }}" class="btn btn-info botao" role="button">--}}
+                                                    {{--<span class="glyphicon glyphicon-star" aria-hidden="true"></span> Apoiar essa Ideia!</a>--}}
+                                            {{--</div>--}}
 
-                                        <div class="pull-right botao">
-                                            @can('edit', $proposal)
-                                            <a href="{{ route('admin.proposal.edit', $proposal->id) }}" class="btn btn-primary botao">
-                                                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar Proposta</a>
-                                            @endcan
 
-                                            @can('bypass', $proposal)
-                                            <a href="{{ route('admin.proposal.bypass', $proposal->id) }}" class="btn btn-danger botao">
-                                                <span class="glyphicon glyphicon-forward" aria-hidden="true"></span> Enviar diretamente à Comissão</a>
-                                            @endcan
+                                            <div class="pull-right botao">
 
-                                            {{--@can('destroy', $proposal)--}}
-                                            {{--<a href="{{ route('admin.proposal.destroy', $proposal->id) }}" class="btn btn-danger botao" onclick="if(!confirm('Você tem certeza que deseja apagar essa Ideia Legislativa?')){return false;};">--}}
-                                                {{--<span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Eliminar Proposta</a>--}}
-                                            {{--@endcan--}}
-                                        </div>
-                                        </div>
+                                                <a href="{{ route('admin.proposal.edit', $proposal->id) }}" class="btn btn-primary botao">
+                                                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar Proposta</a>
+
+                                                @if($proposal->in_committee)
+
+                                                @else
+                                                    <a href="{{ route('admin.proposal.bypass', $proposal->id) }}" class="btn btn-danger botao">
+                                                        <span class="glyphicon glyphicon-forward" aria-hidden="true"></span> Enviar diretamente à Comissão</a>
+                                                @endif
+
+                                                {{--@can('edit', $proposal)
+                                                <a href="{{ route('admin.proposal.edit', $proposal->id) }}" class="btn btn-primary botao">
+                                                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar Proposta</a>
+                                                @endcan
+
+                                                @can('bypass', $proposal)
+                                                <a href="{{ route('admin.proposal.bypass', $proposal->id) }}" class="btn btn-danger botao">
+                                                    <span class="glyphicon glyphicon-forward" aria-hidden="true"></span> Enviar diretamente à Comissão</a>
+                                                @endcan--}}
+
+
+                                                {{--@can('destroy', $proposal)--}}
+                                                {{--<a href="{{ route('admin.proposal.destroy', $proposal->id) }}" class="btn btn-danger botao" onclick="if(!confirm('Você tem certeza que deseja apagar essa Ideia Legislativa?')){return false;};">--}}
+                                                    {{--<span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Eliminar Proposta</a>--}}
+                                                {{--@endcan--}}
+                                            </div>
+                                       </div>
                                     </div>
                                 </div>
                             </div>
