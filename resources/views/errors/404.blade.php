7471529d5ab0eb441f5586b8a60abc0395922778
Felipe Feder <felipeoliveirafeder@hotmail.com>
2017-08-18 18:26:44 +0000

    error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.alerj')
+
+\@section('content')
+
+    <section id="error" class="fundo-azul">
+                <div class="error404-box-body">
+                    <h1><span class="label label-danger">Página não encontrada </span></h1>
+                </div>
+    </section>
+@stop
