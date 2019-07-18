742b4c967e8c64f0ad0761509fcb47013d402eff
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-07-18 18:44:35 -0300

    Update terms

--- resources/views/about/terms.blade.php
+++ resources/views/about/terms.blade.php
@@ -13,7 +13,6 @@
 
 <!-- Current Content -->
 @section('content')
-
     <div class="">
         {{--<div class="panel-heading text-center">Termos de Uso</div>--}}
         <div class="">
@@ -26,22 +25,21 @@
                 O que é
             </h2>
             <div class="termo">
-
                 <p>
-                    O portal Legislaqui oferece um serviço interativo de sugestão de pautas para serem debatidas no parlamento fluminense, que podem virar leis estaduais, através da ferramente da ideia legislativa.
+                    O portal Legislaqui oferece um serviço interativo de sugestão de pautas para serem debatidas no parlamento fluminense, que podem virar leis estaduais, através da ferramenta da ideia legislativa.
                 </p>
                 <h3 class="instrucao-topico-h3">
                     Ideia Legislativa
                 </h3>
                 <p>
-                    Ideias legislativas são enviadas e apoiadas por cidadãos. Elas são sugestões de alteração nas leis estaduais ou mesmo a criação de novas regras. As ideias que receberem 20 mil apoios serão encaminhadas para a Comissão responsável, onde serão objeto de discussão, audiências públicas e receberão um parecer, podendo se tornar projetos de lei.
+                    Ideias legislativas são enviadas e apoiadas por cidadãos. Elas são sugestões de criação ou alteração em leis estaduais. As ideias que receberem 12 mil apoios serão encaminhadas para a Comissão de Normas Internas e Proposições Externas da Alerj, onde serão objeto de discussão, audiências públicas e receberão um parecer, podendo se tornar projetos de lei que vão tramitar na Casa.
                 </p>
                 <hr>
                 <h3 class="instrucao-topico-h3">
                     Cadastro
                 </h3>
                 <p>
-                    A utilização do Legislaqui está condicionado à realização de cadastro por meio de conta no Facebook ou no Google ou, ainda, pelo preenchimento de formulário eletrônico com as informações solicitadas. Neste último caso, o cadastro deverá ser validado conforme instruções na mensagem automática enviada para o e-mail informado.
+                    A utilização do Legislaqui está condicionada à realização de cadastro com inserção de CPF atrelado a conta no Facebook ou no Google ou, ainda, pelo preenchimento de formulário eletrônico com as informações solicitadas. A senha criada para acesso ao portal será criptografada, pessoal e inviolável, não sendo possível à Alerj visualizá-la.
                 </p>
             </div>
 
@@ -49,40 +47,54 @@
                 Atenção
             </h2>
             <div class="termo">
-
                 <p>
                     É proibida a realização de mais de um cadastro por cidadão, bem como a utilização de contas temporárias de e-mail e a utilização da identidade de outra pessoa.
                 </p>
+
                 <p>
                     O Legislaqui não se responsabiliza por erros no cadastro como, por exemplo, cadastro duplo na web, informação errada de dados, entre outros.
                 </p>
-                <p>
-                    Os formulários eletrônicos disponíveis no Portal e-Cidadania somente poderão ser utilizados para envio de informações compatíveis com a destinação das ferramentas e pertinentes à esfera de atuação da Assembleia Legislativa e seus deputados. Assim, não serão aceitos textos que:
-                </p>
-                <p class="instrucao-fundo-cinza">
-                            Tratem de assuntos que não digam respeito ao legislativo estadual;
-                    <br><br>
-                            Contenham mensagens pornográficas, racistas, violentas ou ofensivas à honra, à vida privada, à imagem, à intimidade pessoal e familiar, à ordem pública, à moral, aos bons costumes ou às cláusulas pétreas da Constituição;
-                    <br><br>
-                            Sejam repetidos pelo mesmo usuário ou incompreensíveis.
-                </p>
 
                 <p>
-                    Caso não sejam respeitadas as diretrizes acima, o Legislaqui se reserva o direito de bloquear o cadastro do cidadão e de não publicar o conteúdo enviado, além de bloquear o acesso de determinados endereços IP. Essas e outras providências poderão ser adotadas, ainda, se identificado cadastramento, envio de mensagens ou qualquer outra manifestação originária de sistema automatizado (scripts, bots, etc).
+                    Os formulários eletrônicos somente serão utilizados para envio de informações compatíveis com a destinação das ferramentas e pertinentes à esfera de atuação da Assembleia Legislativa e seus deputados.
                 </p>
+
+                <p><strong>Assim, não serão aceitos textos que:</strong></p>
+
+                <div class="instrucao-fundo-cinza row">
+                    <div class="col-md-10 col-lg-offset-0">
+                        <ul>
+                            <li>Tratem de assuntos que não digam respeito a Políticas Públicas do Estado do Rio de Janeiro;</li>
+                            <li>Tratem de temas de organização interna dos poderes ou órgãos estaduais;</li>
+                            <li>Contenham mensagens pornográficas, racistas, violentas ou ofensivas à honra, à vida privada, à imagem, à intimidade pessoal e familiar, à ordem pública, à moral, aos bons costumes ou às cláusulas pétreas da Constituição;</li>
+                            <li>Sejam repetidos pelo mesmo usuário ou incompreensíveis.</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <p>
+                    Caso não sejam respeitadas as diretrizes acima, o Legislaqui se reserva o direito de não publicar a ideia no sistema, e ainda de bloquear o cadastro do cidadão ou ainda o acesso de determinados endereços IP. Essas e outras providências poderão ser tomadas, ainda, se identificado cadastramento, envio de mensagens ou qualquer outra manifestação originária de sistema automatizado (scripts, bots, etc).
+                </p>
+
                 <p class="instrucao-fundo-cinza">
                     Qualquer conteúdo enviado pelos usuários será moderado pela equipe do Legislaqui. A moderação visa garantir que as condições descritas nestes termos de uso estão sendo respeitadas.
                 </p>
+
                 <p>
-                    Ao se cadastrar, o usuário concorda em receber emails relacionados ao conteúdo do portal. Nos emails enviados será possível cancelar esse recebimento.
+                    <strong>
+                        Ao se cadastrar, o usuário concorda em receber emails relacionados ao portal.
+                    </strong>
                 </p>
+
+                <p>Nos emails enviados será possível cancelar esse recebimento.</p>
+
                 <p class="instrucao-fundo-cinza">
                     As instruções de funcionamento de cada ferramenta disponibilizada no Portal #Legislaaqui, bem como avisos, vídeos explicativos e FAQs, eventualmente publicados, integram o presente termo de uso.
                 </p>
+
                 <p>
                     Qualquer alteração das disposições do presente termo terá efeito imediato e geral a partir do momento de sua publicação, com o devido destaque no portal Legislaqui.
                 </p>
-
             </div>
 
             <h2 class="instrucao-topico">
@@ -93,14 +105,12 @@
 
                 <p>A Alerj reserva-se o direito de divulgar, quando necessário ao funcionamento das ferramentas existentes no Legislaqui, o nome e outras informações referentes à identificação do autor de ideia, comentário, opinião ou qualquer manifestação.</p>
 
-                <p>Os dados pessoais dos cidadãos poderão ser utilizados para fins estatísticos e de relacionamento institucional e servirão para a prestação dos serviços oferecidos pelo portal bem como para seu aprimoramento.</p>
+                <p>Os dados pessoais dos cidadãos podem ser utilizados para fins estatísticos e de relacionamento institucional e servirão para a prestação dos serviços oferecidos pelo portal bem como para seu aprimoramento.</p>
 
                 <p>A utilização do Legislaqui pressupõe autorização para publicação desse conteúdo em quaisquer sites mantidos pela Alerj. O cidadão abre mão de eventuais direitos autorais incidentes sobre suas contribuições e manifestações ao Legislaqui.</p>
 
                 <p>As ideias, comentários, opiniões e quaisquer manifestações dos cidadãos não representam a opinião da Alerj ou seus deputados.</p>
             </div>
-
         </div>
     </div>
-
 @stop
