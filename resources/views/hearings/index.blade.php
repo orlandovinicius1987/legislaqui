8311f47f6f33c30618e343cf0f950e65cef4f8a1
Breno Trengrouse <breno@gmail.com>
2019-07-25 15:06:10 -0300

    buttons links

--- resources/views/hearings/index.blade.php
+++ resources/views/hearings/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.alerj')
+@extends('layouts.camara')
 
 @section('title', config('app.name'))
 
@@ -7,5 +7,9 @@
 
 <!-- Current Proposals -->
 @section('content')
+
+
+    <iframe src="https://edemocracia.al.rj.leg.br/" style="border:0px #ffffff none;" name="myiFrame" scrolling="yes"
+            frameborder="0" marginheight="0px" marginwidth="0px" height="100%" width="100%" allowfullscreen></iframe>
 
 @stop
