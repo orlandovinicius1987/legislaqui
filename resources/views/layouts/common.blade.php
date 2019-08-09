771decb309a41dd159db39ceaa03993a359cb5d8
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-08-09 15:39:45 -0300

    Fix logout

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -57,9 +57,15 @@
                             </a>
                         </li>
                         <li >
-                            <a href="{{ url('/logout') }}">
-                                <i class="fa fa-btn fa-sign-out"></i>Sair
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                Sair
                             </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         </li>
                     @endif
                 </div>
