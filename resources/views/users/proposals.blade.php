a42affa36cd19cb970f9f1f2572b32a73669fd38
Breno Trengrouse <breno@gmail.com>
2019-08-15 15:18:18 -0300

    Search Field

--- resources/views/users/proposals.blade.php
+++ resources/views/users/proposals.blade.php
@@ -63,7 +63,7 @@
                                         <a href="{{ route('proposal.create') }}">
                                             @endif
                                             <div class="icon-wrapper"><i class="fa fa-plus-circle custom-icon"><span class="fix-editor">&nbsp;</span></i></div>
-                                            <div class="quadrado_legislaqui">Crie nova Ideia</div>
+                                            <div class="quadrado_legislaqui">1Crie nova Ideia</div>
                                         </a></h3>
                     </th>
                 </tr>
