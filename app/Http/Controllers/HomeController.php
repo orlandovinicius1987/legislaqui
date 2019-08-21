9e34a31d9bba50f2df4f922aa6e9cb58ab85b7b9
BMasquio <brunomasquio@gmail.com>
2019-08-21 13:43:01 -0300

    Fix filter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Input;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return redirect()->route('proposals.index');
+        return redirect()->route('proposals.index', Input::toArray());
     }
 }
