608c99606f751640e60ff60f55a90cc59e9e37bd
Orlando Vínícius <orlando.vinicius1987@gmail.com>
2020-07-22 15:11:27 +0000

    issue: #205

--- database/migrations/2014_10_11_000001_create_roles_table.php
+++ database/migrations/2014_10_11_000001_create_roles_table.php
@@ -18,7 +18,7 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->integer('id');
+            $table->increments('id');
             $table->string('role');
         });
 
