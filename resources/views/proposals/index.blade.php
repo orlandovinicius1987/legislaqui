62f4d8489970b1612b97a087feb6d696747ba4ed
Breno Trengrouse <breno@gmail.com>
2020-08-28 15:03:00 -0300

    CARDS - Lista Ideia

--- resources/views/proposals/index.blade.php
+++ resources/views/proposals/index.blade.php
@@ -5,6 +5,221 @@
 
 @section('content')
 
+    <div class="cards-lista-ideias">
+        @include('partials.error')
+        <div class="row">
+            <div class="col-md-6 text-left">
+                <h1>Ideias Legislativas</h1>
+            </div>
+
+            <div class="col-md-6 text-right">
+                @if (!Auth::check())
+                    <a class="btn btn-primary" dusk="newProposalButton" href="{{ route('proposal.create') }}" onclick="if(!confirm('Para incluir nova ideia legislativa você deve estar logado')){return false;};">
+                        @else
+                            <a class="btn btn-primary" dusk="newProposalButton" href="{{ route('proposal.create') }}">
+                                @endif
+                                <i class="fa fa-plus-circle"></i> Sua ideia legislativa
+                            </a>
+                    </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <!-- search not in navbar collapse -->
+                <nav class="navbar navbar-expand navbar-light mt-3 mb-4">
+                    <div class="d-flex flex-grow-1">
+                        <form class="my-auto w-100 d-inline-block order-1" action="{{ route('home.post') }}">
+                            <div class="input-group">
+                                <input type="hidden" name="q" value="{{$query}}" >
+                                <input type="text"  name="search" value="{{$search}}" class="form-control border border-right-0" placeholder="Busque uma ideia ...">
+
+                                <span class="input-group-append">
+                                    <button class="btn btn-primary border border-left-0" type="submit" >
+                                        <i class="fas fa-search"></i> Pesquisar
+                                    </button>
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row mb-4 controles">
+            <div class="col-12">
+                <a href="/?q=open"><button type="button" class="btn btn-outline-secondary {{ $query == null ? 'active' :'' }} {{ $query == "open" ? 'active' :'' }}"
+                                           data-toggle="tooltip" data-placement="top"
+                                           title="Essas são as propostas que ainda não chegaram à Comissão. Uma proposta precisa de 20 mil apoios para ser
+                                                  encaminhada à Comissão. Antes de criar uma proposta, verifique se não há uma já criada para o mesmo fim.
+                                                  Várias ideias semelhantes terminam diluindo o apoio dos demais cidadãos.">
+                        ABERTAS</button></a>
+
+                <a href="/?q=committee"><button type="button" class="btn btn-outline-secondary  {{ $query == "committee" ? 'active' :'' }}"
+                                                data-toggle="tooltip" data-placement="top"
+                                                title="Essas são as propostas que receberam o apoio suficiente e, neste momento, estão sendo analisadas pela comissão.">
+                        EM APROVAÇÃO</button></a>
+
+                <a href="/?q=expired"><button type="button" class="btn btn-outline-secondary {{ $query == "expired" ? 'active' :'' }}"
+                                              data-toggle="tooltip" data-placement="top"
+                                              title="Essas são as propostas que não receberam o apoio suficiente e não foram encaminhadas para análise da comissão.">
+                        EXPIRADAS</button></a>
+
+                <a href="/?q=disapproved"><button type="button" class="btn btn-outline-secondary {{ $query == "disapproved" ? 'active' :'' }}"
+                                                  data-toggle="tooltip" data-placement="top"
+                                                  title="Essas são as propostas analisadas e não acatadas pela comissão.">
+                        NÃO ACATADAS</button></a>
+
+                <a href="/?q=approved"><button type="button" class="btn btn-outline-secondary {{ $query == "approved" ? 'active' :'' }}"
+                                               data-toggle="tooltip" data-placement="top"
+                                               title="Essas são as propostas analisadas em tramitação na Comissão.">
+                        EM TRAMITAÇÃO</button></a>
+
+
+                <div class="dropdown float-right">
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        10
+                    </button>
+                    <div class="dropdown-menu text-center" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">25</a>
+                        <a class="dropdown-item" href="#">50</a>
+                        <a class="dropdown-item" href="#">100</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Omnis molestiae possimus in et excepturi.</a></h5>
+                        <p class="card-text">
+                            Alice thought she had felt quite unhappy at the Lizard as she could not help bursting out laughing: and when she had put the Lizard in head downwards, and the baby joined):-- 'Wow! wow! wow!' 'Here!.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary stretched-link"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <span href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Laboriosam dolorem adipisci perferendis id et quis.</a></h5>
+                        <p class="card-text">
+                            WHAT are you?' said the Queen, in a hot tureen! Who for such dainties would not join the dance? Will you, won't you, will you join the dance? "You can really have no answers.' 'If you can't help.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <a href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Omnis molestiae possimus in et excepturi.</a></h5>
+                        <p class="card-text">
+                            Alice thought she had felt quite unhappy at the Lizard as she could not help bursting out laughing: and when she had put the Lizard in head downwards, and the baby joined):-- 'Wow! wow! wow!' 'Here!.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary stretched-link"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <span href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Laboriosam dolorem adipisci perferendis id et quis.</a></h5>
+                        <p class="card-text">
+                            WHAT are you?' said the Queen, in a hot tureen! Who for such dainties would not join the dance? Will you, won't you, will you join the dance? "You can really have no answers.' 'If you can't help.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <a href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Omnis molestiae possimus in et excepturi.</a></h5>
+                        <p class="card-text">
+                            Alice thought she had felt quite unhappy at the Lizard as she could not help bursting out laughing: and when she had put the Lizard in head downwards, and the baby joined):-- 'Wow! wow! wow!' 'Here!.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary stretched-link"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <span href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Laboriosam dolorem adipisci perferendis id et quis.</a></h5>
+                        <p class="card-text">
+                            WHAT are you?' said the Queen, in a hot tureen! Who for such dainties would not join the dance? Will you, won't you, will you join the dance? "You can really have no answers.' 'If you can't help.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <a href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Omnis molestiae possimus in et excepturi.</a></h5>
+                        <p class="card-text">
+                            Alice thought she had felt quite unhappy at the Lizard as she could not help bursting out laughing: and when she had put the Lizard in head downwards, and the baby joined):-- 'Wow! wow! wow!' 'Here!.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary stretched-link"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <span href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Laboriosam dolorem adipisci perferendis id et quis.</a></h5>
+                        <p class="card-text">
+                            WHAT are you?' said the Queen, in a hot tureen! Who for such dainties would not join the dance? Will you, won't you, will you join the dance? "You can really have no answers.' 'If you can't help.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <a href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Omnis molestiae possimus in et excepturi.</a></h5>
+                        <p class="card-text">
+                            Alice thought she had felt quite unhappy at the Lizard as she could not help bursting out laughing: and when she had put the Lizard in head downwards, and the baby joined):-- 'Wow! wow! wow!' 'Here!.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary stretched-link"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <span href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"<a href="http://legislaqui.test/proposals/93">Laboriosam dolorem adipisci perferendis id et quis.</a></h5>
+                        <p class="card-text">
+                            WHAT are you?' said the Queen, in a hot tureen! Who for such dainties would not join the dance? Will you, won't you, will you join the dance? "You can really have no answers.' 'If you can't help.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary"> <i class="fa fa-thumbs-up" aria-hidden="true"></i> 20 Curtidas</a> <a href="#" class="btn btn-outline-primary"><i class="fa fa-star" aria-hidden="true"></i> 69 Apoios</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-12 mt-3">
+                {!! $proposals->links() !!}
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
     <div class="lista-ideias">
 
         @include('partials.error')
@@ -31,7 +246,6 @@
             </div>
 
             <div class="card-body mt-3">
-            <!-- search not in navbar collapse -->
                 <nav class="navbar navbar-expand navbar-light mt-4 ml-3 mr-3">
                     <div class="d-flex flex-grow-1">
                         <form class="my-auto w-100 d-inline-block order-1" action="{{ route('home.post') }}">
@@ -47,10 +261,7 @@
                             </div>
                         </form>
                     </div>
-                    {{--<button class="navbar-toggler order-0" type="button" data-toggle="collapse" data-target="#navbar7">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>--}}
-                </nav>
+                  </nav>
 
 
 
