602618ba318467371b143146d943252e8a0a9321
Breno Trengrouse <breno@gmail.com>
2020-08-28 13:15:44 -0300

    Stick Footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,9 +2,9 @@
 
 
 
-<section id="footer">
+<footer class="footer mt-auto py-3 bg-dark" id="footer">
     <div class="container">
-        <div class="row text-center text-xs-center text-sm-left text-md-left">
+        <div class="row text-center text-xs-center text-sm-left text-md-left pt-3">
             <div class="col-12  col-sm-3 col-md-3">
                 <a href="/" title="Assembléia Legislativa do Estado do Rio de Janeiro">
                     <img src="/imagens/logotipo_inferior.png" class="img-fluid"
@@ -13,15 +13,14 @@
                 </a>
             </div>
             <div class="col-12  col-sm-8 col-md-6 endereco text-center">
-                {{--<h5>PALÁCIO TIRADENTES</h5>--}}
+
                 <p>Rua Primeiro de março, s/n - Praça XV - Rio de Janeiro<br/><span>CEP</span> 20010-090 &nbsp;&nbsp;
-                    <span class="footer_phone">Telefone</span> +55 (21) 2588-1000 &nbsp;&nbsp;{{-- <span>Fax</span> +55 (21) 2588-1516--}}
+                    <span class="footer_phone">Telefone</span> +55 (21) 2588-1000 &nbsp;
                 </p>
 
             </div>
 
             <div class="col-12  col-sm-4 col-md-3">
-                {{--<h5>Quick links</h5>--}}
                 <ul class="list-unstyled list-inline social text-center">
                     <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
                     <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
@@ -31,28 +30,19 @@
             </div>
 
         </div>
-        {{--
-                    <div class="row">
-                        <div class="col-12  col-sm-12 col-md-12 mt-2 mt-sm-5">
-                            <ul class="list-unstyled list-inline social text-center">
-                                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
-                                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
-                                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
-                                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
-                                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
-                            </ul>
-                        </div>
-                        <hr>
-                    </div>
-        --}}
 
         <div class="row pt-5">
             <div class="col-12  col-sm-12 col-md-12 mt-4 mt-sm-2 text-center text-white">
-                {{--<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>--}}
                 <p class="h6">© Todos os direitos reservados.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">ALERJ - Assémbleia Legislativa do Estado do Rio de Janeiro</a></p>
             </div>
             <hr>
         </div>
     </div>
-</section>
+</footer>
+
+
+
+
+
+
 <!-- ./Footer -->
