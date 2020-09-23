67c424c454c838ad882ba26b8e2d8790d9519ce8
ricardocamargo07 <ricardodcstuckert@gmail.com>
2020-09-23 18:43:31 -0300

    .env.travis_config/mail.php

--- config/mail.php
+++ config/mail.php
@@ -54,10 +54,10 @@ return [
     |
     */
 
-    'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+//    'from' => [
+//        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+//        'name' => env('MAIL_FROM_NAME', 'Example'),
+//    ],
 
     /*
     |--------------------------------------------------------------------------
