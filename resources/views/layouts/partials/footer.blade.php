e2771bca54b16c9840e18563dd3d78850cc1907c
Breno Trengrouse <breno@gmail.com>
2020-11-06 15:46:40 -0300

    RESPONSIVE ADJUSTS 002 - Footer Mobile

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,14 +5,14 @@
 <footer class="footer mt-auto py-3 bg-dark" id="footer">
     <div class="container">
         <div class="row text-center text-xs-center text-sm-left text-md-left pt-3">
-            <div class="col-12  col-sm-3 col-md-3">
+            <div class="offset-2 col-8 sm-offset-0 col-sm-3 col-md-3">
                 <a href="/" title="Assembléia Legislativa do Estado do Rio de Janeiro">
                     <img src="/imagens/logotipo_inferior.png" class="img-fluid"
                          alt="Assembléia Legislativa do Estado do Rio de Janeiro"
                          title="Assembléia Legislativa do Estado do Rio de Janeiro">
                 </a>
             </div>
-            <div class="col-12  col-sm-8 col-md-6 endereco text-center">
+            <div class="col-12 col-sm-8 col-md-6 endereco text-center">
 
                 <p>Rua Primeiro de março, s/n - Praça XV - Rio de Janeiro<br/><span>CEP</span> 20010-090 &nbsp;&nbsp;
                     <span class="footer_phone">Telefone</span> +55 (21) 2588-1000 &nbsp;
@@ -20,7 +20,7 @@
 
             </div>
 
-            <div class="col-12  col-sm-4 col-md-3">
+            <div class="col-12 col-sm-4 col-md-3">
                 <ul class="list-unstyled list-inline social text-center">
                     <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
                     <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
@@ -32,7 +32,7 @@
         </div>
 
         <div class="row pt-5">
-            <div class="col-12  col-sm-12 col-md-12 mt-4 mt-sm-2 text-center text-white">
+            <div class="col-12 col-sm-12 col-md-12 mt-0 mt-md-4 text-center text-white copyright">
                 <p class="h6">© Todos os direitos reservados.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">ALERJ - Assémbleia Legislativa do Estado do Rio de Janeiro</a></p>
             </div>
             <hr>
