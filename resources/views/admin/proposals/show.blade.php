56308d3d89a1387801b66b8920c45186868504ed
vagrant <vagrant@homestead>
2020-11-23 18:25:09 +0000

    Fix

--- resources/views/admin/proposals/show.blade.php
+++ resources/views/admin/proposals/show.blade.php
@@ -19,30 +19,6 @@
                             <div class="row">
                                 <div class="col-sm-12">
                                     <div class="panel-body">
-                                        <table class="table-responsive table-striped table-show">
-                                            <tbody>
-                                            <tr><td class="table-td-label-show">Nome</td><td class="table-td-show"> {{ $proposal->user->name }}</td></tr>
-                                            <tr><td class="table-td-label-show">Situação</td><td class="table-td-show"> {{  $proposal->open ? 'Aberta' : 'Fechada'   }}</td></tr>
-                                            @if($proposal->pub_date)
-                                                <tr><td class="table-td-label-show">Data Publicação</td><td class="table-td-show"> {{ $proposal->pub_date->format('d/m/Y') }}</td></tr>
-                                            @else
-                                                <tr><td class="table-td-label-show">Data Publicação<td class="table-td-show"> Exibida após aprovação</td></tr>
-                                            @endif
-                                            <tr><td class="table-td-label-show">Data Limite</td><td class="table-td-show"> {{ $proposal->limit_date->format('d/m/Y') }}</td></tr>
-                                            <tr><td class="table-td-label-show">Exposição da Ideia</td><td class="table-td-show"> {!! $proposal->idea_exposition !!}</td></tr>
-                                            <tr><td class="table-td-label-show">Número de Apoios</td><td class="table-td-show"> {{ $proposal->approvals->count() }}</td></tr>
-                                            <tr></tr>
-                                            {{-- not moderated == null --}}
-                                            <tr><td class="table-td-label-show">Aprovado em</td><td class="table-td-show"> {{ $proposal->approved_at }}</td></tr>
-                                            <tr><td class="table-td-label-show">Aprovado por</td><td class="table-td-show"> {{ $proposal->approved_by }}</td></tr>
-                                            <tr><td class="table-td-label-show">Desaprovado em</td><td class="table-td-show"> {!! $proposal->disapproved_at !!}</td></tr>
-                                            <tr><td class="table-td-label-show">Desaprovado por</td><td class="table-td-show"> {!! $proposal->disapproved_by !!}</td></tr>
-                                            {{-- if has response -> return response data, else null --}}
-                                            <tr><td class="table-td-label-show">Autor Resposta</td><td class="table-td-show"> {{ $proposal->responder ? $proposal->responder->name  : null }}</td></tr>
-                                            <tr><td class="table-td-label-show">Resposta</td><td class="table-td-show"> {!! $proposal->response ? $proposal->response : null !!}</td></tr>
-                                            <tr><td class="table-td-label-show">Projeto de Lei</td><td class="table-td-show"> {!! $proposal->bill_project_id ? $proposal->bill_project_id : 'Ainda não virou Projeto de Lei' !!}</td></tr>
-                                            </tbody>
-                                        </table>
                                         @include('admin.proposals.partials.table')
                                     </div>
 
