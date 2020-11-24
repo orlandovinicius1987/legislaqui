8adf09bcd8ca9616f509026b081e38578889c8b2
BMasquio <brunomasquio@gmail.com>
2020-11-24 17:00:53 +0000

    Tab

--- resources/views/proposals/partials/badge.blade.php
+++ resources/views/proposals/partials/badge.blade.php
@@ -1,42 +1,42 @@
 @switch($proposal->state)
     @case(\App\Enums\ProposalState::Approved)
-    <span class="badge badge-success">
-                                                    Aprovada
-                                                </span>
+        <span class="badge badge-success">
+            Aprovada
+        </span>
     @break
     @case(\App\Enums\ProposalState::Disapproved)
-    <span class="badge badge-danger">
-                                                    Não aprovada
-                                                </span>
+        <span class="badge badge-danger">
+            Não aprovada
+        </span>
     @break
     @case(\App\Enums\ProposalState::Supported)
-    <span class="badge badge-success">
-                                                    Alcançou apoios suficientes
-                                                </span>
+        <span class="badge badge-success">
+            Alcançou apoios suficientes
+        </span>
     @break
     @case(\App\Enums\ProposalState::Expired)
-    <span class="badge badge-danger">
-                                                    Expirada
-                                                </span>
+        <span class="badge badge-danger">
+            Expirada
+        </span>
     @break
     @case(\App\Enums\ProposalState::Sent)
-    <span class="badge badge-success">
-                                                    Enviada para a Comissão de Normas
-                                                </span>
+        <span class="badge badge-success">
+            Enviada para a Comissão de Normas
+        </span>
     @break
     @case(\App\Enums\ProposalState::Forwarded)
-    <span class="badge badge-success">
-                                                    Enviada para projeto de lei
-                                                </span>
+        <span class="badge badge-success">
+            Enviada para projeto de lei
+        </span>
     @break
     @case(\App\Enums\ProposalState::NotForwarded)
-    <span class="badge badge-danger">
-                                                    Não acatada pela Comissão de normas
-                                                </span>
+        <span class="badge badge-danger">
+            Não acatada pela Comissão de normas
+        </span>
     @break
     @case(\App\Enums\ProposalState::BillProject)
-    <span class="badge badge-primary">
-                                                    Virou projeto de lei
-                                                </span>
+        <span class="badge badge-primary">
+            Virou projeto de lei
+        </span>
     @break
 @endswitch
