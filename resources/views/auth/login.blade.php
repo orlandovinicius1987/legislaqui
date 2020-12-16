43eacef1ed10640fd6ef5aa03cfc34271ac62ede
Breno Trengrouse <breno@gmail.com>
2020-12-16 13:17:55 -0300

    Login - Register Clean Code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,8 +41,6 @@
                     <div class="card-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                             {!! csrf_field() !!}
-
-
                             <div class="form-group{{ $errors->has('email') && (Session::get('last_auth_attempt') === 'login') ? ' has-error' : '' }} row">
                                 <div class="col-md-12">
                                     {{--<label for="email" class="col-form-label">Email</label>--}}
@@ -55,7 +53,6 @@
                                     <input id="email" type="email" class="form-control" name="email" value="{{ (Session::get('last_auth_attempt') === 'login') ? old('email') : '' }}" placeholder="Insira seu email">
                                 </div>
                             </div>
-
                             <div class="form-group{{ $errors->has('password') && (Session::get('last_auth_attempt') === 'login') ? ' has-error' : '' }} row">
                                 <div class="col-md-12">
                                     {{--<label for="password" class="col-form-label">Senha</label>--}}
@@ -64,23 +61,18 @@
                                             <strong><i class="fas fa-exclamation-triangle"></i> {{ $errors->first('password') }}</strong>
                                         </span>
                                     @endif
-
                                     <input id="password" type="password" class="form-control" name="password" placeholder="Insira sua senha">
-
                                 </div>
                             </div>
-
                             <div class="form-group row text-left">
                                 <div class="col-md-6 ">
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
                                         <label class="form-check-label" for="remember">
                                             Lembrar de mim
                                         </label>
                                     </div>
                                 </div>
-
                                 <div class="col-md-6 text-right">
                                     @if (Route::has('password.request'))
                                         <a class="forgot-pass " href="{{ route('password.request') }}">
@@ -89,14 +81,11 @@
                                     @endif
                                 </div>
                             </div>
-
                             <div class="form-group row mb-0">
                                 <div class="col-md-12">
-
                                     <button dusk="loginButton" type="submit" class="btn btn-primary btn-block">
                                         <i class="fas fa-sign-in-alt"></i> Entrar
                                     </button>
-
                                 </div>
                             </div>
                         </form>
@@ -105,93 +94,4 @@
             </div>
         </div>
     </div>
-
-
-
-
-
-
-{{--
-    <div class="login">
-        <div class="row">
-            <div class="col-md-5 col-12 ">
-                <div class="panel panel-default panel-bg">
-                    <div class="panel-heading-login">
-                        <div class="col-12  auth text-center">
-                            <h3>
-                                Login
-                            </h3>
-                        </div>
-                        <div class="after-auth">
-                            <h4>
-                                Caso já possua uma conta de usuário, entre com seus dados abaixo.
-                            </h4>
-                        </div>
-                    </div>
-                    <div class="panel-body login-panel">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-
-                            {!! csrf_field() !!}
-
-                            <div class="form-group{{ $errors->has('email') && (Session::get('last_auth_attempt') === 'login') ? ' has-error' : '' }}">
-                                <label class="col-12  col-md-4 control-label">E-Mail</label>
-                                <div class="col-12  col-md-8">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ (Session::get('last_auth_attempt') === 'login') ? old('email') : '' }}" placeholder="Insira seu email">
-                                    @if ($errors->has('email') && (Session::get('last_auth_attempt') === 'login'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('password') && (Session::get('last_auth_attempt') === 'login') ? ' has-error' : '' }}">
-                                <label class="col-12  col-md-4 control-label">Senha</label>
-                                <div class="col-12  col-md-8">
-                                    <input id="password" type="password" class="form-control" name="password" placeholder="Insira sua senha">
-
-                                    @if ($errors->has('password') && (Session::get('last_auth_attempt') === 'login'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-12  col-md-8 col-md-offset-4">
-
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Permanecer conectado
-                                        </label>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-12  col-md-8 col-md-offset-4">
-                                    @if (Route::has('password.request'))
-                                        <a class="forgot-pass " href="{{ route('password.request') }}">
-                                            Esqueceu a sua senha?
-                                        </a>
-                                    @endif
-
-                                    <button dusk="loginButton" type="submit" class="btn btn-primary">
-                                        <i class="fa fa-btn fa-sign-in fa-fw"></i>&nbsp; Entrar
-                                    </button>
-                                </div>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
---}}
-
-
-
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,61 +6,47 @@
         <div class="alert alert-danger">{{ \Session::get('error_msg') }}</div>
     @endif
 
-
-
-
     <div class="d-flex h-100">
         <div class="row align-self-center w-100 login">
             <div class="col-md-8 col-lg-6 mx-auto">
                 <div class="card">
                     <div class="card-header">
-
                         <h3>
                             Registre-se
                         </h3>
-
                         <div class="after-auth">
                             <h5>
                                 Crie uma conta de usuário usando o formulário abaixo.
                             </h5>
                         </div>
-
                     </div>
-
                     <div class="card-body">
-
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                             {!! csrf_field() !!}
-
                             {{ Form::hidden('uuid', Cookie::get('uuid')) }}
-
                             <div class="form-group{{ $errors->has('name') && (\Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }} row">
-                                    <label class="col-12 control-label">Nome</label>
-                                    <div class="col-12 ">
-                                        <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Insira seu nome">
-                                        @if ($errors->has('name') && (Session::get('last_auth_attempt') === 'register'))
-                                            <span class="help-block">
+                                <label class="col-12 control-label">Nome</label>
+                                <div class="col-12 ">
+                                    <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Insira seu nome">
+                                    @if ($errors->has('name') && (Session::get('last_auth_attempt') === 'register'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                        @endif
-                                    </div>
+                                    @endif
+                                </div>
                             </div>
-
                             <div class="form-group{{ $errors->has('email') && (Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }} row">
+                                <label class="col-md-12 control-label">E-Mail</label>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="email" value="{{ (Session::get('last_auth_attempt') === 'register') ? old('email') : '' }}" placeholder="Insira seu email">
 
-                                    <label class="col-md-12 control-label">E-Mail</label>
-
-                                    <div class="col-md-12">
-                                        <input type="text" class="form-control" name="email" value="{{ (Session::get('last_auth_attempt') === 'register') ? old('email') : '' }}" placeholder="Insira seu email">
-
-                                        @if ($errors->has('email') && (Session::get('last_auth_attempt') === 'register'))
-                                            <span class="help-block">
+                                    @if ($errors->has('email') && (Session::get('last_auth_attempt') === 'register'))
+                                        <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                        @endif
-                                    </div>
+                                    @endif
+                                </div>
                             </div>
-
                             <div class="form-group{{ $errors->has('whatsapp') && (\Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }} row">
                                 <label class="col-12 control-label">Whatsapp</label>
                                 <div class="col-12 ">
@@ -74,10 +60,8 @@
                                     @endif
                                 </div>
                             </div>
-
                             <div class="form-group{{ $errors->has('cpf') && (Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }} row">
                                 <label class="col-md-12 control-label">CPF</label>
-
                                 <div class="col-md-12">
                                     <input v-mask='["###.###.###-##"]' required="required" class="form-control" id="cpf" name="cpf" type="text" value="{{ (Session::get('last_auth_attempt') === 'register') ? old('cpf') : '' }}" placeholder="Insira seu CPF">
 
@@ -88,17 +72,12 @@
                                     @endif
                                 </div>
                             </div>
-
-
                             <div class="row form-group{{ $errors->has('password') && (Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }}">
-
                                 <label class="col-md-12 control-label">
                                     Senha
                                 </label>
-
                                 <div class="col-md-12">
                                     <input type="password" class="form-control" name="password" placeholder="Mínimo de 8 dígitos">
-
                                     @if ($errors->has('password') && (Session::get('last_auth_attempt') === 'register'))
                                         <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -106,17 +85,12 @@
                                     @endif
                                 </div>
                             </div>
-
-
                             <div class="row form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-
                                 <label class="col-md-12 control-label">
                                     Confirme a Senha
                                 </label>
-
                                 <div class="col-md-12">
                                     <input type="password" class="form-control" name="password_confirmation" placeholder="Confirmar sua senha">
-
                                     @if ($errors->has('password_confirmation'))
                                         <span class="help-block">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
@@ -124,15 +98,12 @@
                                     @endif
                                 </div>
                             </div>
-
                             <div class="row form-group{{ $errors->has('city_id') ? ' has-error' : '' }}">
                                 <label class="col-md-12 control-label">
                                     {{ Form::Label('Município', 'Município') }}
                                 </label>
-
                                 <div class="col-md-12">
                                     {{ Form::select('city_id', $cities, null, ['data-width'=>'100%','data-style'=>'form-control', 'data-live-search'=>'true', 'title'=>'Escolha um município...']) }}
-
                                     @if ($errors->has('city_id'))
                                         <span class="help-block">
                                         <strong>{{ $errors->first('city_id') }}</strong>
@@ -140,26 +111,16 @@
                                     @endif
                                 </div>
                             </div>
-
                             <input type="hidden" name="uf" value="RJ">
-
                             <div class="row form-group{{ $errors->has('terms') ? ' has-error' : '' }}">
-
                                 <label class="col-md-12 control-label">
-
                                 </label>
-
                                 <div class="col-md-12">
-
                                     <input class="" type="checkbox" name="terms" id="terms" required style="">
-
                                     Li e concordo com os <a href="{{route('terms')}}" target="_blank">Termos de Uso</a> e com a <a href="{{route('privacy-policy')}}" target="_blank">Política de Privacidade</a>.
-
                                 </div>
                             </div>
-
                             @include('partials.recaptcha-v2-form')
-
                             <div class="form-group row mb-0">
                                 <div class="col-md-12">
                                     <button dusk="registerButton" type="submit" class="btn btn-primary">
@@ -167,166 +128,10 @@
                                     </button>
                                 </div>
                             </div>
-
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-
-
-
-
-
-{{--
-
-
-    <!-- Registro -->
-    <div class="col-md-7 col-12 ">
-        <div class="panel panel-default panel-bg">
-            <div class="panel-heading-login">
-                <div class="col-12  auth text-center">
-                    <h3>
-                        Registro TESTE
-                    </h3>
-                </div>
-                <div class="after-auth"><h4>Crie uma conta de usuário usando o formulário abaixo.</h4></div>
-
-            </div>
-
-            <div class="panel-body register">
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                    {!! csrf_field() !!}
-
-                    {{ Form::hidden('uuid', Cookie::get('uuid')) }}
-
-                    <div class="form-group{{ $errors->has('name') && (\Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }}">
-                        <label class="col-12  col-md-4 control-label">Nome</label>
-
-                        <div class="col-12  col-md-8">
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Insira seu nome">
-                            @if ($errors->has('name') && (Session::get('last_auth_attempt') === 'register'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group{{ $errors->has('email') && (Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }}">
-                        <label class="col-12  col-md-4 control-label">E-Mail</label>
-
-                        <div class="col-12  col-md-8">
-                            <input type="text" class="form-control" name="email" value="{{ (Session::get('last_auth_attempt') === 'register') ? old('email') : '' }}" placeholder="Insira seu email">
-
-                            @if ($errors->has('email') && (Session::get('last_auth_attempt') === 'register'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group{{ $errors->has('cpf') && (Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }}">
-                        <label class="col-12  col-md-4 control-label">CPF</label>
-
-                        <div class="col-12  col-md-8">
-                            <input required="required" class="form-control" id="cpf" name="cpf" type="text" value="{{ (Session::get('last_auth_attempt') === 'register') ? old('cpf') : '' }}" placeholder="Insira seu CPF">
-
-                            <input id="cfp" type="text" class="campo" name="cpf" value="{{ (Session::get('last_auth_attempt') === 'register') ? old('cpf') : '' }}" placeholder="Insira seu CPF">
-
-                            @if ($errors->has('cpf') && (Session::get('last_auth_attempt') === 'register'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('cpf') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group{{ $errors->has('password') && (Session::get('last_auth_attempt') === 'register') ? ' has-error' : '' }}">
-                        <label class="col-12  col-md-4 control-label">Senha</label>
-
-                        <div class="col-12  col-md-8">
-                            <input type="password" class="form-control" name="password" placeholder="Mínimo de 8 dígitos">
-
-                            @if ($errors->has('password') && (Session::get('last_auth_attempt') === 'register'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                        <label class="col-12  col-md-4 control-label">Confirme a Senha</label>
-
-                        <div class="col-12  col-md-8">
-                            <input type="password" class="form-control" name="password_confirmation" placeholder="Confirmar sua senha">
-
-                            @if ($errors->has('password_confirmation'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-
-                    <div class="form-group{{ $errors->has('uf') ? ' has-error' : '' }}">
-                        <label class="col-12  col-md-4 control-label">UF</label>
-
-                        <div class="col-12  col-md-8">
-
-                            {{ Form::Label('uf', 'UF:') }}
-
-                            {{ Form::select('uf', $uf, null, ['class' => 'form-control']) }}
-
-                            @if ($errors->has('uf'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('uf') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
-                    </div>
-
-
-
-                --}}
-{{--    {{Location::get()->countryCode}}--}}{{--
-
-
-
-
-
-                    <div class="form-group{{ $errors->register->has('g-recaptcha-response') ? ' has-error text-center' : '' }}">
-                        <div class="col-12 ">
-                            {!! app('captcha')->display() !!}
-
-                            @if ($errors->register->has('g-recaptcha-response'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->register->first('g-recaptcha-response') }}</strong>
-                                    </span>
-                            @endif
-
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-
-                        <div class="col-12  col-md-8 col-md-offset-4">
-                            <button dusk="registerButton" type="submit" class="btn btn-primary">
-                                <i class="fa fa-btn fa-user fa-fw"></i>&nbsp; Registro
-                            </button>
-                        </div>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-    </div>
---}}
-
 @endsection
